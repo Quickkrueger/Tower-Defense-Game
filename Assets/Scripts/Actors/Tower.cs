b1cf6b9408e84ac0e756739a122692ee4b7c01b2
Marshall Krueger <mgkrueger4@gmail.com>
2021-08-24 17:52:40 -0600

    Created Tower and Enemy scripts

--- Assets/Scripts/Actors/Tower.cs
+++ Assets/Scripts/Actors/Tower.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Tower : MonoBehaviour
 {
+    int damage;
+    float range;
+    float attackSpeed;
 
-    int maxHealth;
-    int currentHealth;
-
-    float speed;
     // Start is called before the first frame update
     void Start()
     {
---
+++ Assets/Scripts/Actors/TowerScriptableObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerScriptableObject : ScriptableObject
+{
+    int damage;
+    float range;
+    float attackSpeed;
+
+}
