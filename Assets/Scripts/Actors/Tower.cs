2c14bf44c2c51c49748e34d258966346ef43ccd6
Marshall Krueger <mgkrueger4@gmail.com>
2021-08-26 14:42:30 -0600

    Adding basic data to towers, creating test tower "Basic Tower", making a baseline tower design

--- Assets/Scripts/Actors/Tower.cs
+++ Assets/Scripts/Actors/Tower.cs
@@ -7,11 +7,14 @@ public class Tower : MonoBehaviour
     int damage;
     float range;
     float attackSpeed;
+    public TowerScriptableObject towerData;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        damage = towerData.damage;
+        attackSpeed = towerData.attackSpeed;
+        range = towerData.range;
     }
 
     // Update is called once per frame
