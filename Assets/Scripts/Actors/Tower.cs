9a1de3b2637f898eed1a346629d0d28ca3495e54
Marshall <10923227@uvu.edu>
2021-08-30 09:56:33 -0600

    Adding targeting functionality to towers.

--- Assets/Scripts/Actors/Tower.cs
+++ Assets/Scripts/Actors/Tower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,8 +10,10 @@ public class Tower : MonoBehaviour
     float attackSpeed;
     public TowerScriptableObject towerData;
     public GameObject rangeCollider;
-    GameObject towerModel;
-    GameObject turret;
+    private GameObject towerModel;
+    private GameObject turret;
+    private List<GameObject> targets = null;
+    private GameObject currentTarget = null;
 
     // Start is called before the first frame update
     void Start()
@@ -23,12 +26,44 @@ public class Tower : MonoBehaviour
         towerModel.transform.parent = rangeCollider.transform;
         turret = towerModel.transform.GetChild(0).gameObject;
 
-        
+        targets = new List<GameObject>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         turret.transform.Rotate(0f, 10 * Time.deltaTime, 0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            targets.Add(other.gameObject);
+            if (currentTarget == null)
+            {
+                currentTarget = targets[0];
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            targets.Remove((other.gameObject));
+            if (targets.Count > 0)
+            {
+                if (currentTarget == other.gameObject)
+                {
+                    currentTarget = targets[0];
+                }
+            }
+            else
+            {
+                currentTarget = null;
+            }
+        }
     }
 }
